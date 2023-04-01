0d6b88452056504b96dd8b4e1f3fdbe9537ce1c3
Tanmay_Thawkar <tanmay.thawkar@mitaoe.ac.in>
2023-04-01 20:58:40 +0530

    My  new chatting platform
    This includes:
    Websocket.io &Node.Js with NPM
    CSS,Js&HTML

---
+++ Realtime-Simple-Chat-App-master/server.js
@@ -0,0 +1,19 @@
+const io = require('socket.io')(8093)
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  // console.log('new user')
+  // socket.emit('chat-message','helloworld')
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
